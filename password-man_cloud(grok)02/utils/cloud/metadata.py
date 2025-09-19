bada6008e8f57b2e0cc4fd6159e7cdc5281fe352
dhruc <70601656+dhruc@users.noreply.github.com>
2025-09-19 22:49:58 +0530

    Delete password-man_cloud(grok)02 directory

--- password-man_cloud(grok)02/utils/cloud/metadata.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime
-
-def get_file_metadata(path):
-    return {
-        'size': os.path.getsize(path),
-        'modified': datetime.fromtimestamp(os.path.getmtime(path)).strftime('%Y-%m-%d %H:%M:%S')
-    }
